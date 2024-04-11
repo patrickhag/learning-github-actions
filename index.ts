bfd567168524629da5045b5812e585f2224239d9
Patrick Hagenimana <patrickhag09@gmail.com>
2024-04-11 23:21:02 +0200

    second commit

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
-const a: number = 5;
+const a = 5;
 
 for (let i = 0; i < a; i++) {
   console.log(i);
 }
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
